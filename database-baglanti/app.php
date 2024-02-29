41aff46c97468d236cefba9171525ff397ad37b5
muhammed-ogz <muhammednabioguz@gmail.com>
2024-02-29 12:37:06 +0300

    database time set updated.

--- database-baglanti/app.php
+++ database-baglanti/app.php
@@ -11,8 +11,11 @@ try
 {
     $db = new PDO('mysql:host=localhost;dbname=testdb;', 'root', '12345');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db->query('SET @@lc_time_names = "tr-TR";');   //zaman dilimini türkçeye çevirdik
+    $db->query('SET CHARACTER SET  utf8mb4;');  //database dil setini ayarladık
+    $db->query('SET_CHARACTER_SET_CONNECTION=utf8mb4;');    //bağlantı dil setimizi ayarladık
 }
-catch (PDOException $e) 
+catch (PDOException $e)
 {
     echo $e->getMessage();
 }
