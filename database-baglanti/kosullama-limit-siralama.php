ecb662bd614d53dd2628a2a79da1e5abbb3aed51
muhammed-ogz <muhammednabioguz@gmail.com>
2024-03-02 21:21:12 +0300

    kosullama-limit-siralama updated.

--- database-baglanti/kosullama-limit-siralama.php
+++ database-baglanti/kosullama-limit-siralama.php
@@ -1,12 +1,12 @@
 <?php
 
-try{
-    $db = new PDO('mysql:host=localhost;dbname=testdb','root','12345');
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+try {
+    $db = new PDO('mysql:host=localhost;dbname=testdb', 'root', '12345');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->query('SET @@lc_time_names = "tr_TR"');
     $db->query('SET CHARACTER SET utf8mb4');
     $db->query('SET CHARACTER_SET_CONNECTION=utf8mb4');
-}catch (PDOException $e){
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
 
@@ -20,7 +20,29 @@ echo "<pre>";
 
 //LIKE KULLANIMI
 
-$sql4 = "SELECT * FROM users WHERE username LIKE '%sr%'";
-$q = $db->query($sql4);
+$sql4 = "SELECT username,grup FROM users WHERE username LIKE '%sr%'";
+//$q = $db->query($sql4);
+//$users = $q->fetchAll(PDO::FETCH_ASSOC);
+//
+//echo "<ul>";
+//
+//foreach ($users as $key => $value) {
+//
+//    echo "<li> {$value['username']} adlı kişinin kayıt olduğu gruplar :  {$value['grup']}";
+//
+//}
+//
+//echo "</ul>";
+
+$sql5 = "SELECT MIN(id) as en_kucuk_id, MAX(id) as en_buyuk_id FROM users";
+$in = "SELECT * FROM users WHERE id IN(1,2,3,4)";
+$between = "SELECT * FROM users WHERE id BETWEEN 2 AND 4";
+$q = $db->query($between);
 $users = $q->fetchAll(PDO::FETCH_ASSOC);
+
+
+
 print_r($users);
+
+
+
